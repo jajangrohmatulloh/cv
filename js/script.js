1fe0a7d65e9ae8a6b52282dad9e1f2e4465be001
Jajang Rohmatulloh <jajang.rohmatulloh02@gmail.com>
2022-10-17 14:42:52 +0700

    navfix

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-
+const header = document.getElementsByTagName('header')[0];
+const about = document.getElementById('about');
+document.addEventListener('scroll', function (e) {
+    window.scrollY > about.offsetTop
+        ? header.classList.add('navbar-fixed')
+        : header.classList.remove('navbar-fixed');
+});
