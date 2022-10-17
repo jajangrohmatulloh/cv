3166651429e9d9c144bf8968a992c546a9c82d28
Jajang Rohmatulloh <jajang.rohmatulloh02@gmail.com>
2022-10-17 17:23:19 +0700

    mobile

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 const header = document.getElementsByTagName('header')[0];
 const about = document.getElementById('about');
 document.addEventListener('scroll', function (e) {
-    window.scrollY > about.offsetTop
+    window.scrollY > about.offsetTop - 10
         ? header.classList.add('navbar-fixed')
         : header.classList.remove('navbar-fixed');
+});
+
+const hamburger = document.getElementsByClassName('hamburger')[0];
+
+hamburger.addEventListener('click', function () {
+    hamburger.classList.toggle('toggle');
 });
