d7b02f5e820c25f26860a9f90b45733a0c6a9472
Jajang Rohmatulloh <jajang.rohmatulloh02@gmail.com>
2022-10-18 11:17:59 +0700

    almost done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const header = document.getElementsByTagName('header')[0];
 const about = document.getElementById('about');
+const navbarItems = document.getElementsByClassName('navbar-items')[0];
+
 document.addEventListener('scroll', function (e) {
     window.scrollY > about.offsetTop - 10
         ? header.classList.add('navbar-fixed')
@@ -10,4 +12,5 @@ const hamburger = document.getElementsByClassName('hamburger')[0];
 
 hamburger.addEventListener('click', function () {
     hamburger.classList.toggle('toggle');
+    header.classList.toggle('navbar-items-collapse');
 });
